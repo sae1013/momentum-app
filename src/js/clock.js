2c00c2c4b2b719c88f4cf1b512811669e4e0fd41
minwooJeong <jmw93@naver.com>
2021-06-12 16:15:22 +0900

    Done clock

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,1 +1,15 @@
 const clock = document.querySelector('#clock');
+
+const getClock = () => {
+    const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let seconds = date.getSeconds();
+    hour = hour <10 ? `0${hour}` : hour;
+    minute = minute <10 ? `0${minute}` : minute;
+    seconds = seconds <10 ? `0${seconds}` : seconds;
+    const FormattedTime = `${hour}:${minute}:${seconds}`;
+    clock.innerText = FormattedTime;
+}
+getClock();
+setInterval(getClock,1000);

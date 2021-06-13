4bd1dee5c6a8ac72fd4ece978c39810398f4bb2c
minwooJeong <jmw93@naver.com>
2021-06-13 16:25:57 +0900

    Temperature Update

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,15 +1,32 @@
+import {API_KEY} from '../js/env.js';
+
+const citySpan = document.querySelector('.city');
+const temperatureSpan = document.querySelector('.temperature');
+const cur_temperatureSpan = document.querySelector('.cur-temperature')
+
+const sendWeatherRequest = async(lat,lng)=>{
+    try{
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`);
+        if(!response.ok){
+            throw new Error('fetch Failure');
+        }
+        const data = await response.json();
+        const temp = data.main;
+        citySpan.innerText = data.name;
+        temperatureSpan.innerText= `최고:${Math.floor(temp.temp_max)} °C / 최저:${Math.floor(temp.temp_min)} °C`;
+        cur_temperatureSpan.innerText=`현재:${Math.floor(temp.temp)} °C / 체감:${Math.floor(temp.feels_like)} °C`
+    }catch(err){
+        alert(err.message);
+    }
+}
 const onGeoListener = (pos) => {
     const lat = pos.coords.latitude;
     const lng = pos.coords.longitude;
-    // console.log(lat,lng);
+    sendWeatherRequest(lat,lng); 
 }
 
 const onGeoError = (err)=> {
-    if(err){
-        alert(err.message);
-    }else{
-
-    }
+    alert(err.message);
 }
 
-// navigator.geolocation.getCurrentPosition(onGeoListener,onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoListener,onGeoError);
